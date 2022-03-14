22bdab5560c3c582c8670a097d442910065f8aa1
kongzheng <kongzheng@lixiang.com>
2022-03-14 10:12:32 +0800

    remove useless theme

--- themes/aurora/src/utils/validate.ts
+++
@@ -1,8 +0,0 @@
-export function isExternal(path: string): boolean {
-  return /^(https?:|mailto:|tel:)/.test(path)
-}
-
-export function validUsername(str: string): boolean {
-  const validMap = ['admin', 'editor']
-  return validMap.indexOf(str.trim()) >= 0
-}
